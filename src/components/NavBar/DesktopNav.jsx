7fb1e57575717c271754cd4e9eeb46de12bfe54b
matteomiceli <omatteomiceli@gmail.com>
2022-10-03 23:17:14 -0700

    outside nav click

--- src/components/NavBar/DesktopNav.jsx
+++ src/components/NavBar/DesktopNav.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LinkButton from "../Buttons/LinkButton";
 import arrow_right from "../../images/arrow_forward.svg";
 import Navigation from "./Navigation";
@@ -7,39 +7,48 @@ import Menu from "./Menu";
 const DesktopNav = (props) => {
     const [showMenu, setShowMenu] = useState(false);
     const handleClick = (event) => {
-        // event.preventDefault();
-        setShowMenu(showMenu => !showMenu);
-        // console.log(`showMenu = ${showMenu}`);
-    }
+        setShowMenu(!showMenu);
+    };
 
-    // useEffect(() => {
-    //     const body = document.querySelector('body');
-    //     body.addEventListener('click', (event) => {
-    //         if (showMenu) {
-    //             setShowMenu(false);
-    //         }
-    //     })
-    // }, [showMenu]);
+    const handleOutsideClick = (e) => {
+        if (showMenu) {
+            setShowMenu(false);
+        }
+    };
+
+    useEffect(() => {
+        const outsideNav = document.querySelector(".clickzone");
+        outsideNav.addEventListener("click", handleOutsideClick);
+        return () =>
+            outsideNav.removeEventListener("click", handleOutsideClick);
+    }, [showMenu]);
 
     return (
-        <div id='desktop-nav' className='fade-slide-in'>
-            <LinkButton link="/tickets"
-                        label="Get Tickets"
-                        isInternal={true}
-                        className='transparent'
+        <div id="desktop-nav" className="fade-slide-in">
+            <LinkButton
+                link="/tickets"
+                label="Get Tickets"
+                isInternal={true}
+                className="transparent"
             >
-                <img src={arrow_right} alt="Get your tickets to TEDxSFU 2022 Conference" width="16" height="16"
-                     className="filter-white"/>
+                <img
+                    src={arrow_right}
+                    alt="Get your tickets to TEDxSFU 2022 Conference"
+                    width="16"
+                    height="16"
+                    className="filter-white"
+                />
             </LinkButton>
             <Menu
-                id='menu-toggle'
-                aria-label='Main menu'
-                aria-controls='menu'
+                id="menu-toggle"
+                aria-label="Main menu"
+                aria-controls="menu"
                 aria-expanded={showMenu}
-                handleClick={handleClick}/>
+                handleClick={handleClick}
+            />
             <Navigation
-                id='menu'
-                aria-labelledby='menu-toggle'
+                id="menu"
+                aria-labelledby="menu-toggle"
                 aria-hidden={!showMenu}
                 showMenu={showMenu}
             />
--- src/components/NavBar/Navigation.jsx
+++ src/components/NavBar/Navigation.jsx
@@ -4,23 +4,35 @@ import { Link } from "gatsby";
 import Menu from "../../content/menu";
 
 const Navigation = ({ showMenu, ...rest }) => {
-    const pathname = typeof window !== "undefined" ? window.location.pathname : null;
+    const pathname =
+        typeof window !== "undefined" ? window.location.pathname : null;
     const [active, setActive] = useState(pathname);
     return (
-        <nav className={`${showMenu ? 'show look-through' : 'display-none'}`} {...rest}>
-            {Menu.map((navItem, index) =>
-                <Link to={navItem.href} key={navItem.id}
-                      className={`nav-item ${active === navItem.href ? 'active' : ''}`}
-                      onClick={() => setActive(navItem.href)}
-                      activeClassName='active'>
-                    <div className='menu-description'>
-                        <p className="all-caps">{navItem.title}</p>
-                        <p>{navItem.subtitle}</p>
-                    </div>
-                </Link>
-            )}
-        </nav>
+        <>
+            <nav
+                className={`${showMenu ? "show look-through" : "display-none"}`}
+                {...rest}
+            >
+                {Menu.map((navItem, index) => (
+                    <Link
+                        to={navItem.href}
+                        key={navItem.id}
+                        className={`nav-item ${
+                            active === navItem.href ? "active" : ""
+                        }`}
+                        onClick={() => setActive(navItem.href)}
+                        activeClassName="active"
+                    >
+                        <div className="menu-description">
+                            <p className="all-caps">{navItem.title}</p>
+                            <p>{navItem.subtitle}</p>
+                        </div>
+                    </Link>
+                ))}
+            </nav>
+            <div className={`clickzone ${!showMenu && "display-none"}`}></div>
+        </>
     );
-}
+};
 
 export default Navigation;
