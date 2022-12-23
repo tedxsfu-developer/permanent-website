610c488d2355cae1162cbe95b286a4e25913c113
Celine <hathungn614@gmail.com>
2022-12-22 17:12:44 -0800

    wip

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,27 @@ module.exports = {
         title: `TEDxSFU`,
         siteUrl: `https://www.tedxsfu.com/`,
     },
-    plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-        resolve: 'gatsby-source-filesystem',
-        options: {
-            "name": "images",
-            "path": "./src/images/"
+    plugins: [
+        "gatsby-plugin-sass",
+        "gatsby-plugin-image",
+        "gatsby-plugin-netlify-cms",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "images",
+                "path": "./src/images/"
+            },
+            __key: "images"
         },
-        __key: "images"
-    }]
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                "name": "opportunity",
+                "path": `${__dirname}/src/content/opportunities`
+            },
+        }
+    ]
 };
